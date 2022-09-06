91d0456ecba2161286665676de8a4317eea2c0d7
Avishek Datta Ray <avishek.ray@programmers.io>
2022-09-06 20:49:42 +0530

    Created ExpenseDate component

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import './ExpenseDate.css';
+
+export function ExpenseDate(props) {
+    const [month, date, year] = [
+        props.date.toLocaleString('en-US', { month: 'long' }),
+        props.date.toLocaleString('en-US', { day: '2-digit' }),
+        props.date.getFullYear()
+    ];
+    return (
+        <div className="expense-date">
+            <div className='expense-date__month'>{month}</div>
+            <div className='expense-date__day'>{date}</div>
+            <div className='expense-date__year'>{year}</div>
+        </div>
+    );
+}
