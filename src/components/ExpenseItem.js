37ef31b00532f38e136fd792dcdbf7d99d734c7f
Avishek Datta Ray <avishek.ray@programmers.io>
2022-09-06 19:17:44 +0530

    Understanding props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import './ExpenseItem.css';
 
-export function ExpenseItem() {
+export function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insuarance</h2>
-                <div className="expense-item__price">$ 200</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">$ {props.amount}</div>
             </div>
         </div>
     );
