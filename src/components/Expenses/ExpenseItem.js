0615e3f6c3d0b472827ccf525d67ca6abec95fe4
Avishek Datta Ray <avishek.ray@programmers.io>
2022-09-07 19:24:15 +0530

    understanding basics of state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,17 @@
 import './ExpenseItem.css';
 import { ExpenseDate } from './ExpenseDate';
 import { Card } from '../UI/Card';
+import { useState } from 'react';
 export function ExpenseItem(props) {
     
+    const [title, setTitle] = useState(props.title), clickHandler = () => setTitle(String(Math.random()));
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">$ {props.amount}</div>
+                <button onClick={clickHandler}>Change</button>
             </div>
         </Card>
     );
