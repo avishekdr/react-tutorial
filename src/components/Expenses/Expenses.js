b7c5241fd897c504843b13090e397a72977b40d2
Avishek Datta Ray <avishek.ray@programmers.io>
2022-09-12 10:44:24 +0530

    Implemented loop to render expenses & expense filteration

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,23 @@
 import './Expenses.css';
 import { Card } from '../UI/Card';
 import {ExpenseItem} from './ExpenseItem';
+import { ExpenseFilter } from './ExpenseFilter';
+import { useState } from 'react';
 
 export function Expenses(props) {
+    const [selectedYear, setSelectedYear] = useState('2022'),
+        filterChangeHandler = year => setSelectedYear(year),
+        filteredExpense = props.items.filter(expense => { return expense.date.getFullYear().toString() === selectedYear});
+
     return (
         <Card className="expenses">
-            <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
-            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-            <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
+            <ExpenseFilter selected={selectedYear} onChangeFilter={filterChangeHandler} />
+            {
+                filteredExpense.length === 0 ? <p>No items found</p> :
+                filteredExpense.map(item =>
+                    <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date} />
+                )
+            }
         </Card>
     );
 }
