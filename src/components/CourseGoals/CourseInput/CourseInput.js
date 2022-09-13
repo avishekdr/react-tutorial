79f7d86d57d4e3fc577f7509dcc2c670d0e04c8a
Avishek Datta Ray <avishek.ray@programmers.io>
2022-09-13 07:35:47 +0530

    project base setup

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { Button } from '../../UI/Button/Button';
+import './CourseInput.css';
+
+export function CourseInput(props) {
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const goalInputChangeHandler = event => {
+        setEnteredValue(event.target.value);
+    };
+
+    const formSubmitHandler = event => {
+        event.preventDefault();
+        if(enteredValue.trim().length === 0) { return }
+        props.onAddGoal(enteredValue);
+    };
+
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label>Course Goal</label>
+                <input type="text" onChange={goalInputChangeHandler} />
+            </div>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
+};
---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,9 @@
+import './Button.css';
+
+export function Button(props) {
+  return (
+    <button type={props.type} className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
